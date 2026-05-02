8b950b784a18cc2abfa16821bb22fc59794c8a81
pial6215 <pial15-6215@s.diu.edu.bd>
2026-05-02 23:36:48 +0600

    update allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-unjq-f+f_2=85jz+ag-oonk0y+wq=wek&0$cwp90t_0#=#$*9k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abirpial.pythonanywhere.com']
 
 
 # Application definition
